3dcea0f9602b9be0d0d815c1a76c907946cd4125
luql <5376594@qq.com>
2021-12-31 19:50:31 +0800

    逆波兰计算器

--- chapter4/4.2.c
+++ chapter4/4.2.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <stdio.h>
 double atof(char s[])
 {
 	double val,power;
@@ -18,4 +19,29 @@ double atof(char s[])
 	power *= 10.0;
 	}
 	return sign * val / power;
+}
+
+#define MAXLILNE 100
+
+int main()
+{
+	double sum,atof(char []);
+	char line[MAXLILNE];
+	int getline(char line[],int max);
+	sum = 0;
+	while (getline(line,MAXLILNE) > 0)
+		printf("\t%g\n",sum += atof(line));
+	return 0;
+}
+
+int getline(char s[],int lim)
+{
+	int c,i;
+	i = 0;
+	while (--lim > 0 && ( c = getchar()) != EOF && c != '\n')
+		s[i++] = c;
+	if ( c == '\n')
+		s[i++] = c;
+	s[i] = '\0';
+	return i;
 }
