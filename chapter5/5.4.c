6e24b1ba515e014e945191c8b625120cd8b9f955
luql <5376594@qq.com>
2022-01-18 12:25:20 +0800

    习题作业3、4、5

---
+++ chapter5/5.4.c
@@ -0,0 +1,32 @@
+/*
+ * Exercise 5-4. Write the function strend(s, t), which returns 1 if string t
+ * occurs at the end of the string s, and zero otherwise.
+ *
+ * By Faisal Saadatmand
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+/* functions */
+int strend(char *, char *);
+
+/* strend: returns 1 if t occurs at the end of s, and zero otherwise */
+int strend(char *s, char *t)
+{
+	s += strlen(s) - strlen(t); /* advance the pointer  前进指针 s 指向数组下标0*/
+	while (*s && *t)
+		if (*s++ != *t++)
+			return 0;;
+	return 1;
+}
+
+int main(void)
+{
+	char string1[] = { "this is a string" };
+	char string2[] = { "string" };
+
+	printf("%i\n", strend(string1, string2));
+	
+	return 0;
+}
