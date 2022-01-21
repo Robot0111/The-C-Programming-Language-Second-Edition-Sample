c3ab150ae195b51e2b7c7748c8437937fdc363dd
luql <5376594@qq.com>
2022-01-21 23:05:40 +0800

    命令行参数

---
+++ chapter5/5.10.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(int argc,char *argv[])
+{
+    // while(*argv++){
+    //     printf("%s",*argv);
+    // }
+    while (--argc > 0)
+        printf("%s%s",*++argv,(argc > 1) ? " " : "");
+    printf("\n");
+    return 0;
+    // int i;
+    // for(i = 1; i < argc; i++)
+    //     printf("%s%s",argv[i],(i < argc-1)? " ": "");
+    //     printf("\n");
+    //     return 0;
+}
