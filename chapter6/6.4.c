6b1a6f0f780f5be6585d3d71737319a3f32b668e
luql <5376594@qq.com>
2022-01-26 07:49:06 +0800

    指向结构的指针

---
+++ chapter6/6.4.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define MAXWORD 100
+struct key{
+    char *word;
+    int count;
+}keytab[] = {
+    "auto",0,
+    "break",0,
+    "case",0,
+    "char",0,
+    "const",0,
+    "continue",0,
+    "default",0,
+    "unsigned",0,
+    "void",0,
+    "volatile",0,
+    "while",0
+};
+#define NKEYS (sizeof keytab / sizeof keytab[0])
+int getword(char *,int);
+struct key *binsearch(char *,struct key *,int);
+
+/* 统计关键字的出现次数;采用指针方式实现的版本 */
+
+main()
+{
+    char word[MAXWORD];
+    struct key *p;
+
+    while (getword(word,MAXWORD) != EOF)
+        if (isalpha(word[0]))
+            if((p=binsearch(word,keytab,NKEYS)) != NULL)
+                p->count++;
+    for(p = keytab;p < keytab + NKEYS;p++)
+        if (p -> count > 0)
+            printf("%4d %s\n",p -> count,p -> word);
+    return 0;
+}
+
+struct key *binsearch(char *word,struct key *tab,int n)
+{
+    int cond;
+    struct key *low = &tab[0];
+    struct key *high = &tab[n];
+    struct key *mid;
+
+    while (low < high){
+        mid = low + (high - low) / 2;
+        if ((cond = strcmp(word,mid -> word)) < 0)
+            high = mid;
+        else if (cond > 0)
+            low = mid + 1;
+        else return mid;
+    }
+    return NULL;
+}
