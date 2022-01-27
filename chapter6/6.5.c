3a02a03e43c8ddeb5edf10658259f8077fd3c13d
luql <5376594@qq.com>
2022-01-27 17:21:00 +0800

    二叉树

---
+++ chapter6/6.5.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+struct tnode
+{
+    char *word;
+    int count;
+    struct tnode *left;
+    struct tnoden*right;
+};
+
+
+#define MAXWORD 100
+struct tnode *addtrue(struct tnode *,char *);
+void treeprint(struct tnode *);
+int getword(char *,int);
+
+/* 单词出现斌率的统计 */
+
+main()
+{
+    struct tnode *root;
+    char word[MAXWORD];
+
+    root = NULL;
+    while (getword(word,MAXWORD) != EOF)
+        if (isalpha(word[0]))
+            root = addtree(root,word);
+    treeprint(root);
+    return 0;
+}
+
+struct tnode *talloc(void);
+char *strdup(char *);
+
+/* addtree函数:在p 的位置或p的下方增加一个w节点 */
+
+struct tnode *addtree(struct tnode *p,char *w)
+{
+    int cond;
+
+    if(p == NULL) { /* 该单词是一个新单词 */
+        p = talloc();/* 创建一个新节点 */
+        p -> word = strdup(w);
+        p -> count = 1;
+        p-> left = p->right = NULL;
+    } else if ((cond = strcmp(w,p->word)) == 0)
+        p->count++;         /* 新单词与节点中的单词匹配 */
+    else if (cond < 0)      /* 如果小于该节点中的单词,则进入左子树 */
+        p->left = addtree(p->left,w);
+    else                    /* 如果大于该节点中的单词,则进入右子树 */
+        p->right = addtree(p->right,w);
+    return p;
+}
